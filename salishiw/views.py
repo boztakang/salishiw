151249b8383c7f222a5fd2e8728c3d79750e34e8
dan Mccluskey <danm@dans-MBP.home>
2016-05-25 14:40:40 -0700

    Add header and footer

--- salishiw/views.py
+++ salishiw/views.py
@@ -3,3 +3,7 @@ from django.views import generic
 class HomeView(generic.TemplateView):
     template_name = 'salishiw/home.html'
     
+class HeaderView(generic.TemplateView):
+    template_name = 'salishiw/header.html'
+    context_object_name = 'user'
+    
